0624725af287ed5f229d485bede2a855e7a54c16
skunnath2020 <sskunnath@gmail.com>
2021-02-17 14:57:36 -0500

    WIP

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,19 +10,23 @@ var filterButton= d3.select("#filter-btn");
 var clearButton= d3.select("#clear-btn");
 
 // Getting a reference to the input element on the page with the id property set to 'input-field'
-var inputField = d3.select(".form-control");
+// var inputField = d3.select(".form-control");
+var form = d3.select("#form");
+filterButton.on("click", runEnter);
+form.on("submit", runEnter);
+console.log(form);
 
-function handleClick() {
-    console.log("A button was clicked");
-}
-
-// We can use the `on` function in d3 to attach an event to the handler function
-filterButton.on("click", handleClick);
-// Input fields can trigger a change event when new text is entered.
-inputField.on("change", function() {
-    var newText = d3.event.target.value;
-    console.log("--------------------------------------")
-    console.log(newText);
-  });
-var filteredData = tableData.filter(eventOcc => eventOcc.datetime === inputField);
-console.log(filteredData);
+function runEnter() {
+    //prevent page from refreshing
+    d3.event.preventDefault();
+    //Select the input element and get the raw HTML node
+    var inputElement= d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue= inputElement.property("value");
+    console.log(inputValue)
+    //Filter the date
+    var filteredData = tableData.filter(i => i.datetime === inputValue); 
+    console.log(filteredData)
+    // var dates = filteredData.map(eventDate => eventDate.datetime);
+    // console.log(dates);
+};
